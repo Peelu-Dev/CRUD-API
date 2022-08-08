13f78653bc73efabbbeac21fe36a989a82d5daff
peelu123 <raghuwanshipalash6@gmail.com>
2022-08-08 17:53:06 +0530

    Connected to mysql database

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
+const connection= require("./connection")
 const app = express();
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 module.exports = app;
